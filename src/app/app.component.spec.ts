1b122172d185e087dd4acff88fe234114b0cf122
mahmoud-Riad00 <faydmhmwd1@gmail.com>
2024-09-23 16:17:26 +0300

    a

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Rasclny' title`, () => {
+  it(`should have the 'Recycling' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Rasclny');
+    expect(app.title).toEqual('Recycling');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Rasclny');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Recycling');
   });
 });
